2441f08130335428c289955e0c5b3230d65304a1
YanzhaoW <yannzhaow@gmail.com>
2023-04-21 17:31:48 +0200

    integrate cdash to ci workflow

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -13,12 +13,19 @@
 
 # Dashboard is opened for submissions for a 24 hour period starting at
 # the specified NIGHLY_START_TIME. Time is specified in 24 hour format.
-set(CTEST_PROJECT_NAME "R3BRoot")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
+# set(CTEST_PROJECT_NAME "R3BRoot")
+# set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
+
+# set(CTEST_DROP_METHOD "https")
+# set(CTEST_DROP_SITE "cdash.gsi.de")
+# set(CTEST_DROP_LOCATION "/submit.php?project=R3BRoot")
+# set(CTEST_DROP_SITE_CDASH TRUE)
+
+# set(CTEST_TESTING_TIMEOUT 60)
+set(CTEST_PROJECT_NAME "R3BRoot111")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "https")
-set(CTEST_DROP_SITE "cdash.gsi.de")
-set(CTEST_DROP_LOCATION "/submit.php?project=R3BRoot")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=R3BRoot111")
 set(CTEST_DROP_SITE_CDASH TRUE)
-
-set(CTEST_TESTING_TIMEOUT 60)
