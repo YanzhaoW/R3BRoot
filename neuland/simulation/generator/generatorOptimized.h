41d1c6d8171c45a1315a974f26c9d29f63b03d1b
Paula Ulrich <paula-ulrich@gmx.de>
2024-06-06 13:59:08 +0200

    geratorOptimized

--- neuland/simulation/generator/generatorOptimized.h
+++ neuland/simulation/generator/generatorOptimized.h
@@ -1,13 +1,18 @@
 #pragma once
+#include "RtypesCore.h"
+#include "TRandom.h"
+#include <FairGenerator.h>
 #include <Math/GenVector/Cartesian3D.h>
 #include <Math/GenVector/Polar3D.h>
 #include <Math/GenVector/PxPyPzE4D.h>
+#include <R3BNeulandCommon.h>
 #include <TRandom3.h>
 #include <cmath>
 #include <fmt/format.h>
-
+#include <memory>
+#include "FairPrimaryGenerator.h"
 template <typename AngleDist, typename EnergyDist, typename PointDist>
-class TrackGenerator
+class TrackGenerator : FairGenerator
 {
   public:
     TrackGenerator(AngleDist angle_dist, EnergyDist energy_dist, PointDist point_dist)
@@ -17,98 +22,82 @@ class TrackGenerator
     {
     }
 
-    // void set_detector_width(double width) { detector_width_ = width; }
-    // void set_detector_length(double length) { detector_length_ = length; }
-    // void set_detector_height(double height) { detector_height_ = height; }
     void set_detector_size(double detector_size) { detector_size_ = detector_size; }
-    void set_particle_mass(double particle_mass) { particle_mass_ = particle_mass; }
+    void set_rd_engine(TRandom* user_rd_engine) { rd_engine_ = user_rd_engine; }
 
   private:
-    // double detector_width_{};
-    // double detector_length_{};
-    // double detector_height_{};
-    double particle_mass_{};
     double detector_size_{ 50.0 };
-    static constexpr double speed_of_light_ = 299792458.0;
 
     ROOT::Math::Cartesian3D<double> position_{};
+    ROOT::Math::PxPyPzE4D<double> momentum_energy_{};
 
     AngleDist angle_dist_;
     EnergyDist energy_dist_;
     PointDist point_dist_;
-    TRandom3 rd_engine_;
+    /* TRandom3 rd_engine_; */
+    TRandom* rd_engine_{ gRandom };
 
     auto rd_num_gen_point() -> ROOT::Math::Cartesian3D<double>;
     auto rd_num_gen_angles() -> ROOT::Math::Polar3D<double>;
     auto rd_num_gen_energy() -> double;
     auto calculate_abs_momentum(double kinetic_energy) -> double;
-    auto calculate_momentum_energy(double kinetic_energy, double Theta, double Phi) -> ROOT::Math::PxPyPzE4D<double>;
+    auto calculate_momentum_energy(double kinetic_energy,
+                                   double sin_theta,
+                                   double sin_phi,
+                                   double cos_theta,
+                                   double cos_phi) -> void;
 
-    void calculate_position();
+    void calculate_external_position();
+
+    Bool_t ReadEvent(FairPrimaryGenerator* prim_gen) override
+    {
+        calculate_external_position();
+        prim_gen->AddTrack(13,momentum_energy_.Px(),momentum_energy_.Py(),momentum_energy_.Pz(), position_.X(),  position_.Y(), position_.Z());
+
+        return true;
+    };
 };
 
 template <typename AngleDist, typename EnergyDist, typename PointDist>
 auto TrackGenerator<AngleDist, EnergyDist, PointDist>::rd_num_gen_angles() -> ROOT::Math::Polar3D<double>
 {
     ROOT::Math::Polar3D<double> angles{};
-    angles.SetPhi(rd_engine_.Uniform(0., M_PI));
+    angles.SetPhi(rd_engine_->Uniform(0., M_PI));
     angles.SetTheta(angle_dist_(rd_engine_));
     return angles;
 }
-
-template <typename AngleDist, typename EnergyDist, typename PointDist>
-auto TrackGenerator<AngleDist, EnergyDist, PointDist>::rd_num_gen_energy() -> double
-{
-    return energy_dist_(rd_engine_);
-}
-
-template <typename AngleDist, typename EnergyDist, typename PointDist>
-auto TrackGenerator<AngleDist, EnergyDist, PointDist>::rd_num_gen_point() -> ROOT::Math::Cartesian3D<double>
-{
-
-    return point_dist_(rd_engine_);
-}
-
 template <typename AngleDist, typename EnergyDist, typename PointDist>
 auto TrackGenerator<AngleDist, EnergyDist, PointDist>::calculate_abs_momentum(double kinetic_energy) -> double
 {
-    return std::sqrt(kinetic_energy * kinetic_energy +
-                     2 * particle_mass_ * speed_of_light_ * speed_of_light_ * kinetic_energy);
+    return kinetic_energy / Neuland::CLight;
 }
-
 template <typename AngleDist, typename EnergyDist, typename PointDist>
 auto TrackGenerator<AngleDist, EnergyDist, PointDist>::calculate_momentum_energy(double kinetic_energy,
-                                                                                 double Theta,
-                                                                                 double Phi)
-    -> ROOT::Math::PxPyPzE4D<double>
+                                                                                 double sin_theta,
+                                                                                 double sin_phi,
+                                                                                 double cos_theta,
+                                                                                 double cos_phi) -> void
 {
     ROOT::Math::PxPyPzE4D<double> momentum_energy{ 0, 0, 0, kinetic_energy };
     double abs_momentum = calculate_abs_momentum(kinetic_energy);
-    momentum_energy.SetPx(abs_momentum * std::sin(Theta) * std::cos(Phi));
-    momentum_energy.SetPy(abs_momentum * std::sin(Theta) * std::sin(Phi));
-    momentum_energy.SetPz(abs_momentum * std::cos(Theta));
-    return momentum_energy;
+    momentum_energy_.SetPx(abs_momentum * sin_theta * cos_phi);
+    momentum_energy_.SetPy(abs_momentum * sin_theta * sin_phi);
+    momentum_energy_.SetPz(abs_momentum * cos_theta);
 }
 
 template <typename AngleDist, typename EnergyDist, typename PointDist>
-void TrackGenerator<AngleDist, EnergyDist, PointDist>::calculate_position()
+void TrackGenerator<AngleDist, EnergyDist, PointDist>::calculate_external_position()
 {
-    auto point = rd_num_gen_point();
+    auto point = point_dist_(rd_engine_);
     auto angles = rd_num_gen_angles();
-    auto energy = rd_num_gen_energy();
-    auto momentum_energy = calculate_momentum_energy(energy, angles.Theta(), angles.Phi());
+    auto energy = energy_dist_(rd_engine_);
+    auto sin_phi = std::sin(angles.phi());
+    auto cos_phi = std::cos(angles.phi());
+    auto sin_theta = std::sin(angles.theta());
+    auto cos_theta = std::cos(angles.theta());
+    calculate_momentum_energy(energy, sin_theta, sin_phi, cos_theta, cos_phi);
 
-    if (momentum_energy.Pz() == 0)
-    {
-
-        position_.SetX(point.x() - std::sin(angles.Phi()) * detector_size_);
-        position_.SetY(point.y() - std::cos(angles.Phi()) * detector_size_);
-        position_.SetZ(point.z());
-    }
-    else
-    {
-        position_.SetX(point.x() - std::sin(angles.Theta()) * std::cos(angles.Phi()) * detector_size_);
-        position_.SetY(point.y() - std::sin(angles.Theta()) * std::sin(angles.Phi()) * detector_size_);
-        position_.SetZ(point.z() - std::cos(angles.Theta()) * detector_size_);
-    }
+    position_.SetX(point.x() - sin_theta * cos_phi * detector_size_);
+    position_.SetY(point.y() - sin_theta * sin_phi * detector_size_);
+    position_.SetZ(point.z() - cos_theta * detector_size_);
 }
