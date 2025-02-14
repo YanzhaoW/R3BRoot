7d302861923004fa8f7f7cc38cb723ace80810aa
Yanzhao Wang （王彦昭） <yannzhaow@gmail.com>
2025-02-14 19:16:55 +0100

    Fix the clang-format issues (#29)

--- r3bsource/base/R3BUcesbSource.cxx
+++ r3bsource/base/R3BUcesbSource.cxx
@@ -81,8 +81,7 @@ Bool_t R3BUcesbSource::Init()
     std::ostringstream command;
 
     /* Call ucesb with this command */
-    command << fUcesbPath << " " << fFileName << " "
-            << "--ntuple=" << fNtupleOptions << ",STRUCT,-";
+    command << fUcesbPath << " " << fFileName << " " << "--ntuple=" << fNtupleOptions << ",STRUCT,-";
 
     if (fLastEventNo != -1)
     {
