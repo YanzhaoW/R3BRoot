af160b4288d995b7b980cbfcad09edc50c69fb7c
Yanzhao Wang （王彦昭） <yannzhaow@gmail.com>
2025-07-07 23:57:17 +0200

    Implement TSyncEngine and complete coarse analysis (#41)
    - add R3BParView and map2calpartask to app config
    - add map data converter
    - remove plane/bar number in map data
    - Add BaseParCreator executable
    - add R3BParRootFileIo and detRootFileIo
    - update R3BFileSource2 to pass on EventHeader
    - Add same double plane entry type

--- r3bbase/R3BEventHeader.h
+++ r3bbase/R3BEventHeader.h
@@ -14,6 +14,9 @@
 #pragma once
 
 #include <FairEventHeader.h>
+#include <Rtypes.h>
+#include <TObject.h>
+#include <cstdint>
 #include <stdexcept>
 
 class R3BEventHeader : public FairEventHeader
@@ -55,6 +58,57 @@ class R3BEventHeader : public FairEventHeader
     [[nodiscard]] inline double GetTnext() const { return fTnext; }
 
     // void Register(bool Persistance = true) override{};
+
+    auto operator=(const TNamed& obj) -> R3BEventHeader&
+    {
+        obj.Copy(*this);
+        // auto& event_header = dynamic_cast<R3BEventHeader&>(obj);
+        if (const auto* event_header = dynamic_cast<const FairEventHeader*>(&obj); event_header != nullptr)
+        {
+            SetRunId(event_header->GetRunId());
+            SetEventTime(event_header->GetEventTime());
+            SetInputFileId(event_header->GetInputFileId());
+            SetMCEntryNumber(event_header->GetMCEntryNumber());
+        }
+        if (const auto* event_header = dynamic_cast<const R3BEventHeader*>(&obj); event_header != nullptr)
+        {
+            fExpId = event_header->fExpId;
+            fEventno = event_header->fEventno;
+            fTrigger = event_header->fTrigger;
+            fTimeStamp = event_header->fTimeStamp;
+            fTpat = event_header->fTpat;
+            fTStart = event_header->fTStart;
+            fTStartSimple = event_header->fTStartSimple;
+            fTprev = event_header->fTprev;
+            fTnext = event_header->fTnext;
+        }
+        return *this;
+    }
+
+    void Copy(TObject& obj) const override
+    {
+        TNamed::Copy(obj);
+        // auto& event_header = dynamic_cast<R3BEventHeader&>(obj);
+        if (auto* event_header = dynamic_cast<FairEventHeader*>(&obj); event_header != nullptr)
+        {
+            event_header->SetRunId(GetRunId());
+            event_header->SetEventTime(GetEventTime());
+            event_header->SetInputFileId(GetInputFileId());
+            event_header->SetMCEntryNumber(GetMCEntryNumber());
+        }
+        if (auto* event_header = dynamic_cast<R3BEventHeader*>(&obj); event_header != nullptr)
+        {
+            event_header->fExpId = fExpId;
+            event_header->fEventno = fEventno;
+            event_header->fTrigger = fTrigger;
+            event_header->fTimeStamp = fTimeStamp;
+            event_header->fTpat = fTpat;
+            event_header->fTStart = fTStart;
+            event_header->fTStartSimple = fTStartSimple;
+            event_header->fTprev = fTprev;
+            event_header->fTnext = fTnext;
+        }
+    }
 
   private:
     int fExpId{};
